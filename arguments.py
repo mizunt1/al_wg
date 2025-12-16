ac85943e3556678b59636efa09e05f9b6aeed325
mizunt1 <mizunt@gmail.com>
2025-12-16 15:25:15 -0500

    new experiment / data regimes

--- arguments.py
+++ arguments.py
@@ -14,6 +14,39 @@ class cmnist:
     batch_size_test: int = 64
     n_maj_sources: int = 3
     num_classes: int = 2
+    binary_classification: bool = True
+
+@dataclass
+class cmnist_ood:
+    data_mode: str = 'cmnist_ood'
+    model_name: str = 'BayesianNetRes50ULarger'
+    lr: float = 1e-4
+    batch_size: int = 130
+    num_epochs: int = 50
+    num_majority_points: int = 4000
+    num_minority_points: int = 400
+    al_iters: int  = 100
+    al_size: int = 10
+    batch_size_test: int = 64
+    n_maj_sources: int = 3
+    num_classes: int = 2
+    binary_classification: bool = True
+
+@dataclass
+class cmnist_10:
+    data_mode: str = 'cmnist'
+    model_name: str = 'BayesianNetRes50ULarger'
+    lr: float = 1e-4
+    batch_size: int = 130
+    num_epochs: int = 50
+    num_majority_points: int = 20000
+    num_minority_points: int = 2000
+    al_iters: int  = 100
+    al_size: int = 100
+    batch_size_test: int = 64
+    n_maj_sources: int = 3
+    num_classes: int = 10
+    binary_classification: bool = False
 
 @dataclass
 class wb:
@@ -37,12 +70,12 @@ class celeba:
     lr: float = 1e-6
     batch_size: int = 2
     num_epochs: int = 60
-    num_majority_points: int = 4000
-    num_minority_points: int = 400
+    num_majority_points: int = 15000
+    num_minority_points: int = 300
     al_iters: int  = 100
-    al_size: int = 30
+    al_size: int = 90
     batch_size_test: int = 2
-    n_maj_sources: int = 3
+    n_maj_sources: int = 7
     num_classes: int = 2
 
 @dataclass
@@ -64,7 +97,7 @@ class fmow:
 class camelyon:
     data_mode: str = 'camelyon'
     model_name: str = 'BayesianNetDino'
-    lr: float = 1e-6
+    lr: float = 1e-5
     batch_size: int = 10
     num_epochs: int = 50
     num_majority_points: int = 4000
@@ -74,7 +107,27 @@ class camelyon:
     batch_size_test: int = 10
     n_maj_sources: int = 4
     num_classes: int = 2
-    max_training_data_size: int = 6000
+    group_proportions: list = field(default_factory=lambda: [0.2 for i in range(1)])
+    max_training_data_size: int = None
+
+@dataclass
+class camelyon_ood:
+    data_mode: str = 'camelyon_ood'
+    model_name: str = 'BayesianNetDino'
+    lr: float = 1e-5
+    batch_size: int = 10
+    num_epochs: int = 50
+    num_majority_points: int = 4000
+    num_minority_points: int = 400
+    al_iters: int  = 100
+    al_size: int = 5
+    batch_size_test: int = 10
+    n_maj_sources: int = 4
+    num_classes: int = 2
+    group_proportions: list = [1]
+    group_proportions: list = field(default_factory=lambda: [0.2 for i in range(1)])
+    max_training_data_size: int = None
+    test_source: int = 0
 
 def populate_args_from_dataclass(args, cfg):
     """
